5c7b4b14603b10da815a3156173baff1ef8d8cef
Denze1l <dvornikovd.o.1998@gmail.com>
2019-10-07 17:49:36 +0300

    Updates

---
+++ precache-manifest.37f88c29e10b730ed7a8ec97fcde31ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22bd53d50769902bece73118b2dcc3c",
+    "url": "/goit-react-hw-02-reader/./index.html"
+  },
+  {
+    "revision": "c2fe9f7fbc781282bae7",
+    "url": "/goit-react-hw-02-reader/./static/css/main.c316903e.chunk.css"
+  },
+  {
+    "revision": "842524e14173f49e7c0c",
+    "url": "/goit-react-hw-02-reader/./static/js/2.301b5521.chunk.js"
+  },
+  {
+    "revision": "c2fe9f7fbc781282bae7",
+    "url": "/goit-react-hw-02-reader/./static/js/main.dd9d08ea.chunk.js"
+  },
+  {
+    "revision": "cabe0cd4de8d82f99458",
+    "url": "/goit-react-hw-02-reader/./static/js/runtime-main.b5b7f4be.js"
+  }
+]);
